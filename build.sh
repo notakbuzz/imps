e90e70b82479bb141b6a792a5be9778ba35a6f5d
bunnyy <36611570+bunnyyTheFreak@users.noreply.github.com>
2020-07-17 01:08:58 +0530

    build.sh: some more improvements

--- build.sh
+++ build.sh
@@ -34,61 +34,62 @@ function sync() {
    # It's time to sync!
    git config --global user.name "bunnyyTheFreak"
    git config --global user.email "hsinghalk@yahoo.com"
-   echo -e ${blu} "[*] Syncing sources... This will take a while" ${txtrst}
+   echo -e ${blu} "\n[*] Syncing sources... This will take a while [*]" ${txtrst}
    rm -rf -v .repo/local_manifests
    repo init --depth=1 -u git://github.com/FreakyOS/manifest.git -b still_alive
    repo sync -c -j"$JOBS" --no-tags --no-clone-bundle --force-sync
-   echo -e ${cya} "[*] Syncing sources completed!" ${txtrst}
+   echo -e ${grn} "\n[*] Syncing sources completed! [*]" ${txtrst}
 }
 
 function track_private() {
-   echo -e ${blu} "[*] Fetching private repos..." ${txtrst}
-   sudo rm -rf packages/apps/WallBucket
+   echo -e ${blu} "\n\n[*] Fetching private repos... [*]" ${txtrst}
+   sudo rm -rf -v packages/apps/WallBucket
    git clone "git@github.com:FreakyOS/WallBucket.git" -b test packages/apps/WallBucket
-   echo -e ${cya} "[*] Fetched private repos successfully!" ${txtrst}
+   echo -e ${grn} "\n[*] Fetched private repos successfully! [*]" ${txtrst}
 }
 
 function use_ccache() {
     # CCACHE UMMM!!! Cooks my builds fast
 if [ "$CCACHE" = "true" ]; then
+      echo -e ${blu} "\n\n[*] Enabling cache... [*]" ${txtrst}
       export CCACHE_DIR=/var/lib/jenkins/workspace/jenkins-ccache
       ccache -M 50G
       export CCACHE_EXEC=$(which ccache)
       export USE_CCACHE=1
-      echo -e ${blu} "[*] Yumm! ccache enabled!" ${txtrst}
+      echo -e ${grn} "\n[*] Yumm! ccache enabled! [*]" ${txtrst}
 elif [ "$CCACHE" = "false" ]; then
       export CCACHE_DIR=/var/lib/jenkins/workspace/jenkins-ccache
-      echo -e ${grn} "[*] Ugh! ccache path exported!" ${txtrst}
+      echo -e ${grn} "\n\n[*] Ugh! ccache path exported! [*]" ${txtrst}
 fi
 }
 
 function clean_up() {
   # It's Clean Time
 if [ "$CLEAN" = "true" ]; then
-   echo -e ${blu} "[*] Running clean job - full" ${txtrst}
+   echo -e ${blu} "\n\n[*] Running clean job - full [*]" ${txtrst}
    make clean && make clobber
-   echo -e ${grn}"[*] Clean job completed!" ${txtrst}
+   echo -e ${grn}"\n[*] Clean job completed! [*]" ${txtrst}
 elif [ "$CLEAN" = "false" ]; then
-   echo -e ${blu} "[*] Nothing to clean!" ${txtrst}
+   echo -e ${red} "\n\n[*] Nothing to clean! [*]" ${txtrst}
 fi
 }
 
 function build_main() {
   # It's build time! YASS
    source build/envsetup.sh
-   echo -e ${blu} "[*] Starting the build..." ${txtrst}
+   echo -e ${cya} "\n\n[*] Starting the build... [*]" ${txtrst}
    brunch ${DEVICE}
 }
 
 function build_end() {
   # It's upload time!
-   echo -e ${red} "\n[*] Removing existing cloned ota config directory if any! [*]" ${txtrst}
+   echo -e ${red} "\n\n[*] Removing existing cloned ota config directory if any! [*]" ${txtrst}
    sudo rm -rf -v out/target/product/"$DEVICE"/ota_config 
    echo -e ${grn} "\n[*] Uploading the build! [*]" ${txtrst}
    rsync -azP -v -e ssh out/target/product/"$DEVICE"/FreakyOS*.zip bunnyy@frs.sourceforge.net:/home/frs/project/freakyos/"$DEVICE"/
 #   gdrive upload out/target/product/"$DEVICE"/FreakyOS*.zip   
    echo -e ${blu} "\n[*] Cloning OTA CONFIG for pushing the new json and changelog to gerrit... [*]" ${txtrst}
-   echo -e ${red} "[*] Kindly edit the commit message on the gerrit! [*]" ${txtrst}
+   echo -e ${red} "\n[*NOTE*] Kindly edit the commit message and remove the blank spaces from the changelog on the gerrit! [*NOTE*]" ${txtrst}
    cd out/target/product/"$DEVICE"
    git clone "ssh://bunnyyTheFreak@freakyos.xyz:29418/FreakyOS/ota_config" && scp -p -P 29418 bunnyyTheFreak@freakyos.xyz:hooks/commit-msg "ota_config/.git/hooks/"
    cp -v -f FreakyOS*-Changelog.txt ota_config/"$DEVICE"/"$DEVICE.txt"
